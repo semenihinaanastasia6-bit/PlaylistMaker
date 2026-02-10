c9987b16130709c4c78bf9d12dcc9731cf19db29
Nastya <amirzayev@mycompany.tld>
2026-02-10 11:38:44 +0300

    1

--- app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
@@ -6,11 +6,36 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import com.google.android.material.bottomnavigation.BottomNavigationView // Важно!
+
+import android.content.Intent // Для startActivity
+
 class MediaLibraryActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_media_library)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_search -> {
+                    // Открываем экран поиска
+                    startActivity(Intent(this, SearchActivity::class.java))
+                    true
+                }
+                R.id.action_media -> {
+                    // Остаёмся в текущей активности (медиатека)
+                    true
+                }
+                R.id.action_settings -> {
+                    // Открываем экран настроек
+                    startActivity(Intent(this, SettingsActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
